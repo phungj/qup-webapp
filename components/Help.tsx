72458dd6f66f65dee3d7efeae881b7a78970f8d1
jmanrocks152 <jmanrocks152@gmail.com>
2026-04-26 12:38:57 -0500

    Added ids to skills

--- components/Help.tsx
+++ components/Help.tsx
@@ -5,10 +5,14 @@ import {SKILLS} from "@/src/skills";
 import SkillCard from "@/components/SkillCard";
 
 // TODO: Get this syntax highlighted
+// TODO: Refactor this with a factory
+// TODO: Return to motion animation with this one day
 export default function Help() {
     return (
         <div className="text-center flex flex-col items-center">
-            <TitleItalics/>
+            <div className="mt-5">
+                <TitleItalics/>
+            </div>
             <h1 className="font-title text-heading text-3xl font-bold mt-10 mb-2">What is <span className="text-yellow-300">Q-UP</span>?</h1>
             <h2 className="text-2xl mb-10"><span className="text-yellow-300">Q-UP</span> is <span className="font-bold">the</span> coin flipping eSport where every round is completely fair.</h2>
             <h1 className="font-title text-heading text-3xl font-bold mb-2">What is my goal?</h1>
@@ -16,29 +20,35 @@ export default function Help() {
             <h1 className="font-title text-heading text-3xl font-bold mb-2">How do I obtain Q?</h1>
             <h2 className="text-2xl mb-13">Set up a skill grid and play matches.</h2>
             <h1 className="font-title text-heading text-3xl font-bold mb-2">How do I set up a skill grid?</h1>
-            <h2 className="text-2xl mb-10">Skill grids are loaded using the following JSON specification and coordinate system.</h2>
-            <pre className="bg-base-200 p-4 rounded font-mono text-sm overflow-x-auto text-left mb-5">
-                <code>
-                    {`
-{
-    "skills": [
-        {
-            "id": string,
-            "q": int,
-            "r": int
-        },
-    ]
-}
-                    `}
-                </code>
-            </pre>
-            <Image src={grid}
-                   alt="A pointy-top-up hexagonal grid labeled with coordinates where columns are the first coordinate and positive to the right and the top-left to bottom-right diagonal is the second coordinate and positive to the bottom left."
-                   width={500}
-                   height={500}
-                   className="mb-5"/>
-            <h2 className="text-2xl mb-10">Each grid may only have one skill at each hex and one copy of each skill.</h2>
-            <h1 className="font-title text-heading text-3xl font-bold mb-2">What skills are there?</h1>
+            <h2 className="text-2xl mb-12">Skill grids are loaded using the following JSON specification and coordinate system.</h2>
+            <div className="flex mb-12">
+                <pre className="bg-base-200 p-4 rounded font-mono text-sm overflow-x-auto text-left mb-5">
+                    <code>
+                            {`
+                            
+                            
+                            
+                            
+                            
+    {
+        "skills": [
+            {
+                "id": string,
+                "q": int,
+                "r": int
+            },
+        ]
+    }
+                            `}
+                    </code>
+                </pre>
+                <Image src={grid}
+                       alt="A pointy-top-up hexagonal grid labeled with coordinates where columns are the first coordinate and positive to the right and the top-left to bottom-right diagonal is the second coordinate and positive to the bottom left."
+                       width={500}
+                       height={500}/>
+            </div>
+            <h2 className="text-2xl mb-12">Each grid may only have one skill at each hex and one copy of each skill.</h2>
+            <h1  className="font-title text-heading text-3xl font-bold mb-2">What skills are there?</h1>
             <div className="grid grid-cols-3 gap-4">
                 {SKILLS.map((skill, i) => (
                     <SkillCard key={i} skill={skill} />
--- components/SkillCard.tsx
+++ components/SkillCard.tsx
@@ -9,6 +9,7 @@ export default function SkillCard({skill}: SkillCardProps) {
         <div className="card">
             <div className="card-body items-center text-center">
                 <h2 className="card-title">{skill.name}</h2>
+                <h3 className="text-gray-500 italic text-xs">ID: {skill.id}</h3>
                 <h2>{skill.trigger}</h2>
                 <h2>{skill.description}</h2>
             </div>
